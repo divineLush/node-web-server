6be3cba3ca213011ca29b1ff9bf6dfd422b84fd0
divineLush <homeoflucifer@gmail.com>
2021-03-24 12:15:52 +0700

    routing

--- app.js
+++ app.js
@@ -7,9 +7,23 @@ var fs = require('fs');
 // the server object emits an event
 http.createServer(function(request, response) {
     // http response starts with what's is in the head
-    // writeHead(status code, headers)
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    // response is a stream
-    fs.createReadStream(__dirname + '/index.htm').pipe(response);
+
+    switch(request.url) {
+        case '/':
+            // response is a stream so I can pipe it
+            fs.createReadStream(__dirname + '/index.htm').pipe(response);
+            break;
+        case '/api':
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            var obj = {
+                firstName: 'John',
+                lastName: 'Doe',
+            };
+            response.end(JSON.stringify(obj));
+            break;
+        default:
+            response.writeHead(404);
+            response.end();
+    }
 }).listen(1337, '127.0.0.1');
 // .listen(port, hostname)
