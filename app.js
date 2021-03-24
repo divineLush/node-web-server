33e27a959f230891769160ea8fdc351f63d39d26
divineLush <homeoflucifer@gmail.com>
2021-03-24 11:47:11 +0700

    refactor using streams

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ http.createServer(function(request, response) {
     // http response starts with what's is in the head
     // writeHead(status code, headers)
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
-    var message = 'Hello world';
-    html = html.replace('{Message}', message);
-    response.end(html);
+    // response is a stream
+    fs.createReadStream(__dirname + '/index.htm').pipe(response);
 }).listen(1337, '127.0.0.1');
 // .listen(port, hostname)
